77752f0706ae9ed674a6561a8ec430b72c779e3a
Vladislav Drobovich <vladdrobovich@gmail.com>
2018-09-27 00:44:03 +0300

    good night

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+	// your implementation
+	var zeroCount = 0;
+  
+	for(var j=1;number/Math.pow(5,j)>=1;j++){
+		zeroCount+=Math.floor(number/Math.pow(5,j));
+	}
+	
+return zeroCount;
+
 }
